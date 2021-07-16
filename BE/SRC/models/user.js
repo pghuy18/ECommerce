f8bc3acb77ce58b04e2f4221ede35fe263402d7c
Phạm Gia Huy <pghuy18@apcs.vn>
2021-07-16 10:32:36 +0700

    bye

--- BE/SRC/models/user.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose")
-
-const userSchema = mongoose.Schema({
-    username: { required: true, type: String },
-    password: { required: true, type: String },
-})
-
-module.exports = mongoose.model('user', userSchema)
