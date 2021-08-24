58c40254e80b702f61d964eaa4bac070729aab65
Phạm Gia Huy <pghuy18@apcs.vn>
2021-08-24 19:47:27 +0700

    commit coffeeholic

--- BE/SRC/models/order.js
+++ BE/SRC/models/order.js
@@ -1,81 +1,30 @@
-const { TooManyRequests } = require("http-errors")
-const mongoose = require("mongoose")
+const Order = require("./model").Order;
+const Cart = require("./model").Cart;
+const selectFields = ["-createdAt", "-updatedAt"];
+const populateOpt = {
+  path: "item",
+  select: selectFields,
+  populate: {
+    path: "info",
+    select: selectFields,
+    populate: { path: "category", select: ["name"] },
+  },
+};
+module.exports.createOrder = async (o, cartId) => {
+  try {
+    let ret = await new Order(o).save();
+    let cartDelete = await Cart.findByIdAndDelete(cartId);
+    if (ret && cartDelete) return ret;
+  } catch (err) {
+    throw new Error(err);
+  }
+};
 
-const contactSchema = mongoose.Schema({
-    contacter: { required: true, type: String },
-    email: { required: true, type: String },
-    tel: { required: true, type: String }
-})
-//main
-const orderSchema = mongoose.Schema({
-    userID: { required: true, type: mongoose.SchemaTypes.ObjectId },
-    luggageID: { required: true, type: mongoose.SchemaTypes.ObjectId },
-    serviceID: { required: true, type: mongoose.SchemaTypes.ObjectId },
-    flightID: { required: true, type: mongoose.SchemaTypes.ObjectId },
-    contact: { required: true, type: contactSchema },
-    shuttleID: { required: true, type: mongoose.SchemaTypes.ObjectId },
-    price: { required: true, type: Number, default: 0 },
-    passenger: { required: true, type: Array },
-    status: { required: true, type: String, default: 'red'}
-}, { collection: 'orders' }) //sửa database
-//---------------------------------------------------------------------------
-
-//flight
-const flightSchema = mongoose.Schema({
-    airplaneID: { required: true, type: mongoose.SchemaTypes.ObjectId },
-    from: { required: true, type: mongoose.SchemaTypes.ObjectId },
-    to: { required: true, type: mongoose.SchemaTypes.ObjectId },
-    departureTime: { required: true, type: String },
-    landingTime: { required: true, type: String },
-    price: { required: true, type: Number, default: 0 },
-}, { collection: 'flight' })
-
-const airlineSchema = mongoose.Schema({
-    name: { required: true, type: String }
-}, { collection: 'airline' })
-
-const airplaneSchema = mongoose.Schema({
-    airlineID: { required: true, type: mongoose.SchemaTypes.ObjectId },
-    name: { required: true, type: String }
-}, { collection: 'airplane' })
-
-const citySchema = mongoose.Schema({
-    name: { required: true, type: String }
-}, { collection: 'city' })
-
-const airportSchema = mongoose.Schema({
-    cityID: { required: true, type: mongoose.SchemaTypes.ObjectId },
-    name: { required: true, type: String },
-}, { collection: 'airport' })
-//----------------------------------------------------------------------
-
-//additional information
-const luggageSchema = mongoose.Schema({
-    name: { required: true, type: String },
-    fee: { required: true, type: Number, default: 0 }
-}, { collection: 'luggage' })
-const serviceSchema = mongoose.Schema({
-    name: { required: true, type: String },
-    fee: { required: true, type: Number, default: 0 }
-}, { collection: 'service' })
-const shuttleSchema = mongoose.Schema({
-    name: { required: true, type: String }
-}, { collection: 'shuttle' })
-const classSchema = mongoose.Schema({
-    name: { required: true, type: String },
-    fee: { required: true, type: Number, default: 0 }
-}, { collection: 'class' })
-//------------------------------------------------------------------------
-module.exports = {
-    order: mongoose.model('order', orderSchema),
-    flight: mongoose.model('flight', flightSchema),
-    airline: mongoose.model('airline', airlineSchema),
-    airplane: mongoose.model('airplane', airplaneSchema),
-    city: mongoose.model('city', citySchema),
-    airport: mongoose.model('airport', airportSchema),
-    luggage: mongoose.model('luggage', luggageSchema),
-    service: mongoose.model('service', serviceSchema),
-    shuttle: mongoose.model('shuttle', shuttleSchema),
-    planeclass: mongoose.model('planeclass', classSchema),
-    contact: mongoose.model('contact', contactSchema)
-}
+module.exports.getOrderById = async (id) => {
+  try {
+    let ret = await Order.findById(id).populate(populateOpt);
+    return ret;
+  } catch (err) {
+    throw new Error(err);
+  }
+};
--- BE/SRC/routes/index.js
+++ BE/SRC/routes/index.js
@@ -1,64 +1,19 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const user = require('../models/user') // ==>12
-    /* GET users listing. */
-const { registerValidation, loginValidation } = require("../auth/validation");
-const jwt = require("jsonwebtoken")
-const verify = require('../auth/checkToken')
-const bcrypt = require("bcryptjs");
+var axios = require("axios");
 
-router.post('/register', async(req, res) => {
-    const { error } = registerValidation(req.body);
-    if (error) return res.status(400).send(error.details[0].message)
-    const salt = await bcrypt.genSalt(10);
-    const hashPass = await bcrypt.hash(req.body.password, salt)
+const app = require("../models/app.js");
+const chalk = require("chalk");
+const helper = require("../helper");
+const home = require("../controllers/home");
 
-    // Tạo user
-    const newuser = new user();
-    newuser.username = req.body.username
-    newuser.password = hashPass
+router.get("/covid", home.covid);
+router.get("/search:q?", home.search);
+router.get("/detail/:id?", home.detail);
+router.get("/shop/:p?", home.shop);
+router.get("/aboutus/:coffee?", home.aboutus);
+router.get("/error/auth", home.auth);
+router.get("/error/authorize", home.authorize);
+router.get("/", home.index);
 
-    const usernameExist = await user.findOne({ username: req.body.username });
-    if (usernameExist) return res.status(400).send("username not available")
-
-    else
-        try {
-            const User = await newuser.save()
-            console.log("Save on dtb successfull");
-            return res.status(200).send(User);
-        } catch (err) {
-            console.log("Promise Rejected");
-            return res.status(400).send(err);
-        }
-})
-
-router.post('/login', async function(req, res) {
-    const { error } = loginValidation(req.body);
-    if (error)
-        return res.status(400).send(error.details[0].message)
-    const userLogin = await user.findOne({ username: req.body.username });
-    if (!userLogin)
-        return res.status(400).send("Cant find username")
-
-    // Kiểm tra password
-    const passLogin = await bcrypt.compare(req.body.password, userLogin.password);
-    if (!passLogin)
-        return res.status(400).send("Password not correct")
-
-    // Tạo token 
-    const token = jwt.sign({ _id: userLogin._id }, process.env.SECRET_TOKEN)
-    return res.status(200).header("auth-token", token).send(token);
-})
-
-router.get('/:username', async (req, res) => {
-    try {
-        const User = await user.findOne({username: req.params.username});
-        if (!User)
-            return res.status(404).send("User not found");
-        return res.status(200).send(User);
-    } catch (err) {
-        return res.status(400).send(err)
-    }
-})
-
-module.exports = router
+module.exports = router;
